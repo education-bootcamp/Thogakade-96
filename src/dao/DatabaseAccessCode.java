567fb1848e5846e168b1654655639465d3fcb934
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 09:28:58 +0530

    6

--- src/dao/DatabaseAccessCode.java
+++ src/dao/DatabaseAccessCode.java
@@ -2,9 +2,22 @@ package dao;
 
 import entity.Customer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseAccessCode {
 // save Customer
-    public boolean saveCustomer(Customer c){
-
+    public boolean saveCustomer(Customer c) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade",
+                "root","1234");
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO Customer VALUES(?,?,?,?)");
+        statement.setString(1,c.getId());
+        statement.setString(2,c.getName());
+        statement.setString(3,c.getAddress());
+        statement.setDouble(4,c.getSalary());
+        return statement.executeUpdate()>0;
     }
 }
