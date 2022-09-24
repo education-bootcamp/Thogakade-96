31fd1b1cb6af34ecc422d3394d5abe7ddc0498bf
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 09:10:03 +0530

    5

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+    public Button btnSaveCustomer;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
