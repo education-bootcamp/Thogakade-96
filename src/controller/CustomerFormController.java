8079530219e768c36fa8f93311185041282b5ee1
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 09:42:34 +0530

    6

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import dao.DatabaseAccessCode;
+import entity.Customer;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class CustomerFormController {
     public TextField txtId;
@@ -12,5 +17,19 @@ public class CustomerFormController {
     public Button btnSaveCustomer;
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1= new Customer(
+                txtId.getText(),txtName.getText(),txtAddress.getText(),Double.parseDouble(txtSalary.getText())
+        );
+        try {
+            boolean isCustomerSaved = new DatabaseAccessCode().saveCustomer(c1);
+            if (isCustomerSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"Customer Saved").show();
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Try Again").show();
+            }
+        }catch (SQLException | ClassNotFoundException e){
+            new Alert(Alert.AlertType.ERROR,"Something went wrong!").show();
+            e.printStackTrace();
+        }
     }
 }
