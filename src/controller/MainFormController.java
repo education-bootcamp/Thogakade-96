7d4d74ef329b6c9ba512b8aa5a66322ba1a8ba5f
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 08:48:05 +0530

    2

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public AnchorPane mainFormContext;
+
+    public void btnItemManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerManagementOnAction(ActionEvent actionEvent) {
+    }
 }
