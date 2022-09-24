1b2d2dd3036a8523fd7aeb0e97152c432232e9f6
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 09:05:22 +0530

    4

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,7 +1,12 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
     public AnchorPane mainFormContext;
@@ -9,6 +14,13 @@ public class MainFormController {
     public void btnItemManagementOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnCustomerManagementOnAction(ActionEvent actionEvent) {
+    public void btnCustomerManagementOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("CustomerForm");
+    }
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) mainFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(
+                getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
     }
 }
