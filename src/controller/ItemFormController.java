950780b793a11d717e776e62a5680fb71d3d41f0
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-09-24 11:05:13 +0530

    12

--- src/controller/ItemFormController.java
+++ src/controller/ItemFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class ItemFormController {
+    public AnchorPane itemFormContext;
+    public TextField txtCode;
+    public TextField txtDescription;
+    public TextField txtUnitPrice;
+    public TextField txtQtyOnHand;
+    public Button btnSaveItem;
+    public TableView tblItem;
+    public TableColumn colCode;
+    public TableColumn colDescription;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colOption;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void newItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveItemOnAction(ActionEvent actionEvent) {
+    }
 }
